dd679bcc6ba6ac7b9971cff2f33258922e5c75a3
Your Name <you@example.com>
2021-09-25 12:23:39 +0530

    ready to make changes

--- app.js
+++ app.js
@@ -170,6 +170,7 @@ app.use((err, req, res, next) => {
 }
 );  
 
-app.listen(3000, () => {
-  console.log('listening on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`serving on port ${port}`);
 })
